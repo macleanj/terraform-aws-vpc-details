d010054e135e807955c46025ed28276dc17ad280
Jerome Mac Lean <jerome.maclean@serimaconsulting.com>
2019-07-18 21:46:01 +0200

    Added additional subnet attributes

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
 output "nr_of_azs_private" {
   value = "${local.nr_of_azs_private}"
 }
-
 output "nr_of_azs_public" {
   value = "${local.nr_of_azs_public}"
 }
@@ -9,65 +8,122 @@ output "nr_of_azs_public" {
 output "subnet_private1_id" {
   value = "${local.subnet_private1_id}"
 }
-
+output "subnet_private1_availability_zone" {
+  value = "${local.subnet_private1_availability_zone}"
+}
+output "subnet_private1_availability_zone_id" {
+  value = "${local.subnet_private1_availability_zone_id}"
+}
 output "subnet_private1_cidr_block" {
   value = "${local.subnet_private1_cidr_block}"
 }
-
+output "subnet_private1_ipv6_cidr_block" {
+  value = "${local.subnet_private1_ipv6_cidr_block}"
+}
 output "subnet_private2_id" {
   value = "${local.subnet_private2_id}"
 }
-
+output "subnet_private2_availability_zone" {
+  value = "${local.subnet_private2_availability_zone}"
+}
+output "subnet_private2_availability_zone_id" {
+  value = "${local.subnet_private2_availability_zone_id}"
+}
 output "subnet_private2_cidr_block" {
   value = "${local.subnet_private2_cidr_block}"
 }
-
+output "subnet_private2_ipv6_cidr_block" {
+  value = "${local.subnet_private2_ipv6_cidr_block}"
+}
 output "subnet_private3_id" {
   value = "${local.subnet_private3_id}"
 }
-
+output "subnet_private3_availability_zone" {
+  value = "${local.subnet_private3_availability_zone}"
+}
+output "subnet_private3_availability_zone_id" {
+  value = "${local.subnet_private3_availability_zone_id}"
+}
 output "subnet_private3_cidr_block" {
   value = "${local.subnet_private3_cidr_block}"
 }
-
+output "subnet_private3_ipv6_cidr_block" {
+  value = "${local.subnet_private3_ipv6_cidr_block}"
+}
 output "subnet_private4_id" {
   value = "${local.subnet_private4_id}"
 }
-
+output "subnet_private4_availability_zone" {
+  value = "${local.subnet_private4_availability_zone}"
+}
+output "subnet_private4_availability_zone_id" {
+  value = "${local.subnet_private4_availability_zone_id}"
+}
 output "subnet_private4_cidr_block" {
   value = "${local.subnet_private4_cidr_block}"
 }
-
+output "subnet_private4_ipv6_cidr_block" {
+  value = "${local.subnet_private4_ipv6_cidr_block}"
+}
 
 
 output "subnet_public1_id" {
   value = "${local.subnet_public1_id}"
 }
-
+output "subnet_public1_availability_zone" {
+  value = "${local.subnet_public1_availability_zone}"
+}
+output "subnet_public1_availability_zone_id" {
+  value = "${local.subnet_public1_availability_zone_id}"
+}
 output "subnet_public1_cidr_block" {
   value = "${local.subnet_public1_cidr_block}"
 }
-
+output "subnet_public1_ipv6_cidr_block" {
+  value = "${local.subnet_public1_ipv6_cidr_block}"
+}
 output "subnet_public2_id" {
   value = "${local.subnet_public2_id}"
 }
-
+output "subnet_public2_availability_zone" {
+  value = "${local.subnet_public2_availability_zone}"
+}
+output "subnet_public2_availability_zone_id" {
+  value = "${local.subnet_public2_availability_zone_id}"
+}
 output "subnet_public2_cidr_block" {
   value = "${local.subnet_public2_cidr_block}"
 }
-
+output "subnet_public2_ipv6_cidr_block" {
+  value = "${local.subnet_public2_ipv6_cidr_block}"
+}
 output "subnet_public3_id" {
   value = "${local.subnet_public3_id}"
 }
-
+output "subnet_public3_availability_zone" {
+  value = "${local.subnet_public3_availability_zone}"
+}
+output "subnet_public3_availability_zone_id" {
+  value = "${local.subnet_public3_availability_zone_id}"
+}
 output "subnet_public3_cidr_block" {
   value = "${local.subnet_public3_cidr_block}"
 }
-
+output "subnet_public3_ipv6_cidr_block" {
+  value = "${local.subnet_public3_ipv6_cidr_block}"
+}
 output "subnet_public4_id" {
   value = "${local.subnet_public4_id}"
 }
-
+output "subnet_public4_availability_zone" {
+  value = "${local.subnet_public4_availability_zone}"
+}
+output "subnet_public4_availability_zone_id" {
+  value = "${local.subnet_public4_availability_zone_id}"
+}
 output "subnet_public4_cidr_block" {
   value = "${local.subnet_public4_cidr_block}"
+}
+output "subnet_public4_ipv6_cidr_block" {
+  value = "${local.subnet_public4_ipv6_cidr_block}"
 }
