260a9316c96ac14f66560dda7120f29a2f049216
Jerome Mac Lean <jerome.maclean@serimaconsulting.com>
2019-07-18 18:31:46 +0200

    Update of variables

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
- data "aws_vpc" "selected" {
+data "aws_vpc" "selected" {
   filter {
     name   = "name"
     values = ["${var.vpc_name}"]
@@ -101,28 +101,28 @@ locals {
   nr_of_azs_private = "${length(data.aws_subnet_ids.public.ids)}"
   nr_of_azs_public  = "${length(data.aws_subnet_ids.public.ids)}"
 
-  subnet_private1_id = "${length(data.aws_subnet.private1.*.id) > 0 ? element(concat(data.aws_subnet.private1.*.id, list("")), 0) : "Not existing"}"
-  subnet_private1_cidr_block  = "${length(data.aws_subnet.private1.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.private1.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_private1_id         = "${length(data.aws_subnet.private1.*.id) > 0 ? element(concat(data.aws_subnet.private1.*.id, list("")), 0) : "Not existing"}"
+  subnet_private1_cidr_block = "${length(data.aws_subnet.private1.*.cidr_block) > 0 ? element(concat(data.aws_subnet.private1.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_private2_id = "${length(data.aws_subnet.private2.*.id) > 0 ? element(concat(data.aws_subnet.private2.*.id, list("")), 0) : "Not existing"}"
-  subnet_private2_cidr_block  = "${length(data.aws_subnet.private2.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.private2.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_private2_id         = "${length(data.aws_subnet.private2.*.id) > 0 ? element(concat(data.aws_subnet.private2.*.id, list("")), 0) : "Not existing"}"
+  subnet_private2_cidr_block = "${length(data.aws_subnet.private2.*.cidr_block) > 0 ? element(concat(data.aws_subnet.private2.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_private3_id = "${length(data.aws_subnet.private3.*.id) > 0 ? element(concat(data.aws_subnet.private3.*.id, list("")), 0) : "Not existing"}"
-  subnet_private3_cidr_block  = "${length(data.aws_subnet.private3.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.private3.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_private3_id         = "${length(data.aws_subnet.private3.*.id) > 0 ? element(concat(data.aws_subnet.private3.*.id, list("")), 0) : "Not existing"}"
+  subnet_private3_cidr_block = "${length(data.aws_subnet.private3.*.cidr_block) > 0 ? element(concat(data.aws_subnet.private3.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_private4_id = "${length(data.aws_subnet.private4.*.id) > 0 ? element(concat(data.aws_subnet.private4.*.id, list("")), 0) : "Not existing"}"
-  subnet_private4_cidr_block  = "${length(data.aws_subnet.private4.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.private4.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_private4_id         = "${length(data.aws_subnet.private4.*.id) > 0 ? element(concat(data.aws_subnet.private4.*.id, list("")), 0) : "Not existing"}"
+  subnet_private4_cidr_block = "${length(data.aws_subnet.private4.*.cidr_block) > 0 ? element(concat(data.aws_subnet.private4.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_public1_id = "${length(data.aws_subnet.public1.*.id) > 0 ? element(concat(data.aws_subnet.public1.*.id, list("")), 0) : "Not existing"}"
-  subnet_public1_cidr_block  = "${length(data.aws_subnet.public1.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.public1.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_public1_id         = "${length(data.aws_subnet.public1.*.id) > 0 ? element(concat(data.aws_subnet.public1.*.id, list("")), 0) : "Not existing"}"
+  subnet_public1_cidr_block = "${length(data.aws_subnet.public1.*.cidr_block) > 0 ? element(concat(data.aws_subnet.public1.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_public2_id = "${length(data.aws_subnet.public2.*.id) > 0 ? element(concat(data.aws_subnet.public2.*.id, list("")), 0) : "Not existing"}"
-  subnet_public2_cidr_block  = "${length(data.aws_subnet.public2.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.public2.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_public2_id         = "${length(data.aws_subnet.public2.*.id) > 0 ? element(concat(data.aws_subnet.public2.*.id, list("")), 0) : "Not existing"}"
+  subnet_public2_cidr_block = "${length(data.aws_subnet.public2.*.cidr_block) > 0 ? element(concat(data.aws_subnet.public2.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_public3_id = "${length(data.aws_subnet.public3.*.id) > 0 ? element(concat(data.aws_subnet.public3.*.id, list("")), 0) : "Not existing"}"
-  subnet_public3_cidr_block  = "${length(data.aws_subnet.public3.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.public3.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_public3_id         = "${length(data.aws_subnet.public3.*.id) > 0 ? element(concat(data.aws_subnet.public3.*.id, list("")), 0) : "Not existing"}"
+  subnet_public3_cidr_block = "${length(data.aws_subnet.public3.*.cidr_block) > 0 ? element(concat(data.aws_subnet.public3.*.cidr_block, list("")), 0) : "Not existing"}"
 
-  subnet_public4_id = "${length(data.aws_subnet.public4.*.id) > 0 ? element(concat(data.aws_subnet.public4.*.id, list("")), 0) : "Not existing"}"
-  subnet_public4_cidr_block  = "${length(data.aws_subnet.public4.*.cidr_block ) > 0 ? element(concat(data.aws_subnet.public4.*.cidr_block , list("")), 0) : "Not existing"}"
+  subnet_public4_id         = "${length(data.aws_subnet.public4.*.id) > 0 ? element(concat(data.aws_subnet.public4.*.id, list("")), 0) : "Not existing"}"
+  subnet_public4_cidr_block = "${length(data.aws_subnet.public4.*.cidr_block) > 0 ? element(concat(data.aws_subnet.public4.*.cidr_block, list("")), 0) : "Not existing"}"
 
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "vpc_name" {
   description = "Name of the VPC"
-  default          = "${var.vpc_name}"
 }
 
 variable "subnet_name_private1" {
@@ -9,14 +8,17 @@ variable "subnet_name_private1" {
 
 variable "subnet_name_private2" {
   description = "Name of Private Subnet 2"
+  default = ""
 }
 
 variable "subnet_name_private3" {
   description = "Name of Private Subnet 3"
+  default = ""
 }
 
 variable "subnet_name_private4" {
   description = "Name of Private Subnet 4"
+  default = ""
 }
 
 
@@ -26,12 +28,15 @@ variable "subnet_name_public1" {
 
 variable "subnet_name_public2" {
   description = "Name of Public Subnet 2"
+  default = ""
 }
 
 variable "subnet_name_public3" {
   description = "Name of Public Subnet 3"
+  default = ""
 }
 
 variable "subnet_name_public4" {
   description = "Name of Public Subnet 4"
+  default = ""
 }
