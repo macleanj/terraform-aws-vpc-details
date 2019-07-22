a97704eb8f2a9f95b4a8cf235073cafd3813d83f
Jerome Mac Lean <jerome.maclean@serimaconsulting.com>
2019-07-22 19:55:20 +0200

    Update example and aligned readme

--- README.md
+++ README.md
@@ -22,13 +22,14 @@ provider "aws" {
 module "vpc_details" {
   source               = "git::https://github.com/macleanj/terraform_aws_vpc_details.git"
   vpc_name             = "my-great-vpc"
-  subnet_name_private1 = "us-east-1a"
-  subnet_name_private2 = "us-east-1b"
 
-  subnet_name_public1 = "us-east-1a"
-  has_nat_gw_public1  = true
-  subnet_name_public2 = "us-east-1b"
-  has_nat_gw_public2  = true
+  subnet_name_private1 = "${local.region}a"
+  subnet_name_private2 = "${local.region}b"
+
+  subnet_name_public1 = "${local.region}a"
+  has_nat_gw_public1  = false
+  subnet_name_public2 = "${local.region}b"
+  has_nat_gw_public2  = false
 }
 ```
 ## Inputs
--- example/example.tf
+++ example/example.tf
@@ -8,19 +8,13 @@ provider "aws" {
 
 module "vpc_details" {
   source               = "git::https://github.com/macleanj/terraform_aws_vpc_details.git"
-  # vpc_name             = "my-great-vpc"
-  vpc_name             = "Beanstalk beanstalk (dev)"
+  vpc_name             = "my-great-vpc"
+
   subnet_name_private1 = "${local.region}a"
   subnet_name_private2 = "${local.region}b"
-  # subnet_name_private2 = "${local.region}c"
-  # subnet_name_private2 = "${local.region}d"
 
   subnet_name_public1 = "${local.region}a"
-  # has_nat_gw_public1  = true
+  has_nat_gw_public1  = false
   subnet_name_public2 = "${local.region}b"
-  # has_nat_gw_public2  = true
-  # subnet_name_public3 = "${local.region}c"
-  # has_nat_gw_public3  = true
-  # subnet_name_public4 = "${local.region}d"
-  # has_nat_gw_public4  = true
+  has_nat_gw_public2  = false
 }
